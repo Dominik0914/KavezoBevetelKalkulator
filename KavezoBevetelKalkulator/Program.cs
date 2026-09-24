cd01147e95c51eabf4dfdd08268c82d277d7429d
Dominik0914 <rozsa.dominik.2024d@telekimezotur.hu>
2026-09-24 12:56:33 +0200

    5.feladat

--- KavezoBevetelKalkulator/Program.cs
+++ KavezoBevetelKalkulator/Program.cs
@@ -3,9 +3,16 @@ List<int> bevetel = new List<int>();
 Console.WriteLine("=== Kávézó Heti Bevételek Rögzítése ===");
 for(int i=0; i<5;i++)
 {
-    Console.Write($"{i+1}. nap bevétele (Ft)");
+    Console.Write($"{i+1}. nap bevétele (Ft): ");
     int napi = int.Parse(Console.ReadLine());
     bevetel.Add(napi);
+}
+Console.WriteLine("Adatok feldolgozása...");
+Console.WriteLine("========================================");
+Console.WriteLine("Rögzített napi bevételek:");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"\t {i + 1}.nap {bevetel[i]} Ft");
 }
 //3.feladat
 int osszeg = 0;
@@ -14,6 +21,7 @@ for (int i = 0; i < 5; i++)
    osszeg += bevetel[i];
 }
 double atlag = osszeg / 5.0;
+Console.WriteLine("----------------------------------------");
 Console.WriteLine($"Összesített bevétel: {osszeg} Ft");
 Console.WriteLine($"Napi átlagbevétel: {atlag:F2} Ft");
 //4.feladat
@@ -30,4 +38,5 @@ else
 {
     status = "Gyenge hét,felülviszgálat szükséges";
 }
-Console.WriteLine($"Heti státusz {status}");
+Console.WriteLine($"Heti státusz: {status}");
+Console.WriteLine("========================================");
